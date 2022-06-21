d7cad7780deb3707573e2e548e81dd8ebf6f103b
Arssin <mateusz.binieda@onet.pl>
2022-06-21 18:45:58 +0200

    feature: add rotues

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
 import './Welcome.scss'
 
 export function Welcome() {
     return (
         <div className="welcome">
             <h1 className="title">Welcome</h1>
-            <a href="/" className="goToStartbrn">
+            <Link to="/start" className="goToStartbrn">
                 Go to starting page
-            </a>
+            </Link>
         </div>
     )
 }
