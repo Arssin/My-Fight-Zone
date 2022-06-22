bb6d44f0aead2e6f07554c0edebfe399445d54ef
Arssin <mateusz.binieda@onet.pl>
2022-06-22 16:06:09 +0200

    feature: news carousel

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,51 @@
+import { useState } from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import bjj from '../../images/BJJ.png'
+import sherdog from '../../images/Sherdog.png'
+import wrestling from '../../images/Wrestling.jpg'
+import './Home.scss'
+
 export function Home() {
-    return <div>HomePage</div>
+    const [index, setIndex] = useState(0)
+
+    const handleSelect = (selectedIndex: number) => {
+        setIndex(selectedIndex)
+    }
+
+    return (
+        <div>
+            <Carousel
+                activeIndex={index}
+                onSelect={handleSelect}
+                className="carousel-container"
+            >
+                <Carousel.Item className="carouselItem">
+                    <img className="picture" src={bjj} alt="First slide" />
+                    <div className="shadow" />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carouselItem">
+                    <img
+                        className="picture"
+                        src={wrestling}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="carouselItem">
+                    <img className="picture" src={sherdog} alt="Third slide" />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <div>Newsy</div>
+        </div>
+    )
 }
