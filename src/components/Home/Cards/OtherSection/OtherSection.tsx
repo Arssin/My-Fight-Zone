192788cb7499722fe15bab73e8e8b103a2ce75fc
Arssin <mateusz.binieda@onet.pl>
2022-07-07 11:47:58 +0200

    Change colors, change carousel idicator

--- src/components/Home/Cards/OtherSection/OtherSection.tsx
+++ src/components/Home/Cards/OtherSection/OtherSection.tsx
@@ -1,5 +1,10 @@
 import './OtherSection.scss'
 
 export function OtherSection() {
-    return <div className="secondSection">Essa</div>
+    return (
+        <div className="secondSection">
+            <div className="sectionDivs">AD</div>
+            <div className="sectionDivs">Upcoming Fights</div>
+        </div>
+    )
 }
