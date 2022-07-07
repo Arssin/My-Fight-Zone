ddf7d131d4c04f5b8c704a578c19e62b5801ba54
Arssin <mateusz.binieda@onet.pl>
2022-07-07 11:29:16 +0200

    Feature: add variables for scss, change home layout

--- src/components/Home/Cards/Cards.tsx
+++ src/components/Home/Cards/Cards.tsx
@@ -9,72 +9,75 @@ import './Cards.scss'
 export function Cards() {
     return (
         <div className="card-container">
-            <CardGroup className="card-group-one">
-                <Card>
-                    <Card.Img variant="top" src={pictureCard3} />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a
-                            natural lead-in to additional content. This content
-                            is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">
-                            Last updated 3 mins ago
-                        </small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src={pictureCard} />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural
-                            lead-in to additional content.{' '}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">
-                            Last updated 3 mins ago
-                        </small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
-            <CardGroup className="card-group-two">
-                <Card>
-                    <Card.Img variant="top" src={pictureCard4} />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a
-                            natural lead-in to additional content. This content
-                            is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">
-                            Last updated 3 mins ago
-                        </small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src={pictureCard6} />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural
-                            lead-in to additional content.{' '}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">
-                            Last updated 3 mins ago
-                        </small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
+            <div className="cardSection">
+                <CardGroup className="card-group-one">
+                    <Card>
+                        <Card.Img variant="top" src={pictureCard3} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below
+                                as a natural lead-in to additional content. This
+                                content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">
+                                Last updated 3 mins ago
+                            </small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={pictureCard} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural
+                                lead-in to additional content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">
+                                Last updated 3 mins ago
+                            </small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+                <CardGroup className="card-group-two">
+                    <Card>
+                        <Card.Img variant="top" src={pictureCard4} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below
+                                as a natural lead-in to additional content. This
+                                content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">
+                                Last updated 3 mins ago
+                            </small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={pictureCard6} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural
+                                lead-in to additional content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted">
+                                Last updated 3 mins ago
+                            </small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+            </div>
+            <div className="secondSection">Essa</div>
         </div>
     )
 }
