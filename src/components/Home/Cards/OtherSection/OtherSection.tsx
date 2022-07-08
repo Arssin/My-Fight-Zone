4410c715b2121aa4f01b16706dea339a65325541
Arssin <mateusz.binieda@onet.pl>
2022-07-08 18:50:38 +0200

    Add icons, and react icons

--- src/components/Home/Cards/OtherSection/OtherSection.tsx
+++ src/components/Home/Cards/OtherSection/OtherSection.tsx
@@ -1,3 +1,4 @@
+import { BsLinkedin, BsGithub, BsFacebook, BsInstagram } from 'react-icons/bs'
 import sts from '../../../../images/stslogo.png'
 import './OtherSection.scss'
 
@@ -5,11 +6,20 @@ export function OtherSection() {
     return (
         <div className="secondSection">
             <div className="sectionDivs">
+                <div>Follow us!</div>
+                <div>
+                    <BsLinkedin />
+                    <BsGithub />
+                    <BsFacebook />
+                    <BsInstagram />
+                </div>
+            </div>
+            <div className="sectionDivs">
                 <a href="https://www.sts.pl/">
                     <img className="ad" src={sts} alt="bet365 logo" />
                 </a>
             </div>
-            <div className="sectionDivs">Upcoming Fights</div>
+            <div className="sectionDivs">Champions</div>
         </div>
     )
 }
