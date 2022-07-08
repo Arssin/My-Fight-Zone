960139871a7805d9b26a2f1a877de2a98990325b
Arssin <mateusz.binieda@onet.pl>
2022-07-08 18:38:16 +0200

    add section with ads

--- src/components/Home/Cards/OtherSection/OtherSection.tsx
+++ src/components/Home/Cards/OtherSection/OtherSection.tsx
@@ -1,9 +1,14 @@
+import sts from '../../../../images/stslogo.png'
 import './OtherSection.scss'
 
 export function OtherSection() {
     return (
         <div className="secondSection">
-            <div className="sectionDivs">AD</div>
+            <div className="sectionDivs">
+                <a href="https://www.sts.pl/">
+                    <img className="ad" src={sts} alt="bet365 logo" />
+                </a>
+            </div>
             <div className="sectionDivs">Upcoming Fights</div>
         </div>
     )
