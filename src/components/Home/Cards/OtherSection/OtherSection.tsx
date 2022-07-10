7f3b8be64bf7c18f6a54cc8eee33c0081725d189
Arssin <mateusz.binieda@onet.pl>
2022-07-10 13:36:49 +0200

    fetching data with ufc api

--- src/components/Home/Cards/OtherSection/OtherSection.tsx
+++ src/components/Home/Cards/OtherSection/OtherSection.tsx
@@ -1,4 +1,5 @@
 import { BsLinkedin, BsGithub, BsFacebook, BsInstagram } from 'react-icons/bs'
+import { DataFetching } from '../../../../data/ufcranking'
 import sts from '../../../../images/stslogo.png'
 import githublogo from '../../../../images/githublogo.png'
 import './OtherSection.scss'
@@ -42,6 +43,7 @@ export function OtherSection() {
                 </div>
             </div>
             <div className="sectionDivs">
+                <p className="text">Ads</p>
                 <a href="https://www.sts.pl/" target="_blank" rel="noreferrer">
                     <img className="ad" src={sts} alt="sts logo" />
                 </a>
@@ -53,7 +55,10 @@ export function OtherSection() {
                     <img className="ad" src={githublogo} alt="github logo" />
                 </a>
             </div>
-            <div className="sectionDivs">Champions</div>
+            <div className="sectionDivs">
+                <p> Current UFC Champions</p>
+                <DataFetching />
+            </div>
         </div>
     )
 }
