bb6dc4320d446fbbc77aee1adbd14d537edf443d
Arssin <mateusz.binieda@onet.pl>
2022-07-10 15:08:40 +0200

    Add blogs

--- src/components/Blog/Texts/TextsBlog.tsx
+++ src/components/Blog/Texts/TextsBlog.tsx
@@ -1,5 +1,23 @@
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import './TextsBlog.scss'
 
 export function TextsBlog() {
-    return <div>TextsBlog</div>
+    return (
+        <div>
+            <p>TextBlog</p>
+            <Card className="text-center">
+                <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to
+                        additional content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            </Card>
+        </div>
+    )
 }
