b7e106f9ca152fd9ef764ae5246053dd7d3c34a1
Arssin <mateusz.binieda@onet.pl>
2022-07-10 12:38:56 +0200

    Add github logo

--- src/components/Home/Cards/OtherSection/OtherSection.tsx
+++ src/components/Home/Cards/OtherSection/OtherSection.tsx
@@ -1,5 +1,6 @@
 import { BsLinkedin, BsGithub, BsFacebook, BsInstagram } from 'react-icons/bs'
 import sts from '../../../../images/stslogo.png'
+import githublogo from '../../../../images/githublogo.png'
 import './OtherSection.scss'
 
 export function OtherSection() {
@@ -15,8 +16,20 @@ export function OtherSection() {
                 </div>
             </div>
             <div className="sectionDivs">
-                <a href="https://www.sts.pl/" target="_blank" rel="noreferrer">
+                <a
+                    href="https://www.sts.pl/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="anchors"
+                >
                     <img className="ad" src={sts} alt="sts logo" />
+                </a>
+                <a
+                    href="https://github.com/Arssin"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img className="ad" src={githublogo} alt="github logo" />
                 </a>
             </div>
             <div className="sectionDivs">Champions</div>
